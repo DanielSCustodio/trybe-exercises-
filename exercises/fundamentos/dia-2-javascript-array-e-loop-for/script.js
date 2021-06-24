59c42b7d2c8c662e09e96d51e9969ad1c6bb2146
danielm <play.daniel@hotmail.com>
2021-06-24 19:00:13 -0300

    #23 - Somando todos os valores de um array

--- exercises/fundamentos/dia-2-javascript-array-e-loop-for/script.js
+++ exercises/fundamentos/dia-2-javascript-array-e-loop-for/script.js
@@ -1,6 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let n of numbers) {
+/*for (let n of numbers) {
     console.log(n);
-}
+
+}*/
+let total = 0;
+/*for (let index = 0; index < numbers.length; index+=1) {
+total += numbers[index];
+}*/
 
