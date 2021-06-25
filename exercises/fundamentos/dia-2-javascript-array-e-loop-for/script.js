4eb385384ff4d12a25608a13f933de1741f9a810
danielm <play.daniel@hotmail.com>
2021-06-24 21:57:29 -0300

    #27 - Verificando quantidade de número ímpares existente em um array

--- exercises/fundamentos/dia-2-javascript-array-e-loop-for/script.js
+++ exercises/fundamentos/dia-2-javascript-array-e-loop-for/script.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 7, 8, 100, 2, 35, 27];
 
 /*for (let n of numbers) {
     console.log(n);
@@ -41,3 +41,15 @@ for (let index = 0; index < numbers.length; index+=1) {
 }
 
 console.log(maior);*/
+/*contImpar = 0;
+for (let index = 0; index < numbers.length; index+=1) {
+	if (numbers[index] % 2 !== 0) {
+		contImpar+=1
+	}
+	
+	
+}
+if (contImpar === 0) {
+	console.log("nenhum valor ímpar encontrado");
+}
+console.log(contImpar); */
