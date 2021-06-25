b2decce60c09f2949613197b04544e0e44ca0104
danielm <play.daniel@hotmail.com>
2021-06-25 19:26:59 -0300

    #32 -Manipulação de impressão usando for #2

--- exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,14 +1,19 @@
 n = 5;
 let ast ="";
 
-for (let index = 0; index < n; index += 1) {
-		ast += "*";
-}
-for (let index = 0; index < n; index++) {
+// for (let index = 0; index < n; index += 1) {
+// 		ast += "*";
+// }
+// for (let index = 0; index < n; index++) {
+// 	console.log(ast);
+	
+// }
+
+for (let index = 0; index < n; index+=1) {
+	ast += "*";
 	console.log(ast);
 	
 }
-
 
 
 
