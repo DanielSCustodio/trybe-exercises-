bca16aa8a07bf4942b0199c8e0e827de20340710
danielm <play.daniel@hotmail.com>
2021-06-29 01:56:28 -0300

    #43 - Verificação do número que mais se repete em um array

--- exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia 4.4 | JavaScript - Objetos e funções/exercicio-2.js
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia 4.4 | JavaScript - Objetos e funções/exercicio-2.js
@@ -9,19 +9,24 @@
 //  }
 // verificaPalindrome("arara");
 
-// let numbers = [2, 3, 6, 7, 10, 1];
-// let maior = numbers[0];
-// let indiceMaior;
-// for (let index=0; index < numbers.length; index += 1){
+// let numeros = [2, 3, 6, 7, 10, 1];
+// function maiorIndice(numbers){
+// 	let maior = numbers[0];
+// 	let indiceMaior;
+// 	for (let index=0; index < numbers.length; index += 1){
 // 	if (numbers[index] > maior) {
 // 		maior = numbers[index];
 // 		indiceMaior = index
 // 	}
 // }
 // console.log(indiceMaior);
+// }
 
-// let numbers = [2, 4, 6, 7, 10, 0, -3];
-// let menor = numbers[0];
+// maiorIndice(numeros);
+
+// let numeros= [2, 4, 6, 7, 10, 0, -3];
+// function menorIndice(numbers){
+// 	let menor = numbers[0];
 // let indiceMenor;
 // for (let index=0; index < numbers.length; index += 1){
 // 	if (numbers[index] < menor) {
@@ -30,9 +35,13 @@
 // 	}
 // }
 // console.log(indiceMenor);
+// }
 
-// let nome = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
-// let maior = nome[0].length;
+// menorIndice(numeros);
+
+// let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+// function maiorNome(nome){
+// 	let maior = nome[0].length;
 // let maiorNome;
 
 // for (let index = 0; index < nome.length; index+=1) {
@@ -43,32 +52,37 @@
 
 // }
 // console.log(maiorNome);
+// }
+
+// maiorNome(nomes);
 
 
-let numbers = [2, 3, 2, 5, 8, 2, 3];
-let igual =0;
-let numeroRepetido= 0;
-let indexGanhador =0;
-let vezesRepete =0;
+// let n = [2, 3, 2, 5, 8, 2, 3];
 
-for (let index = 0; index < numbers.length; index+=1) {
-	for (let i in numbers) {
-		if (numbers[index] === numbers[i]) {
-			igual +=1;
-			vezesRepete = igual;
-			}
-		}	
-		igual =0;
-		if (vezesRepete > numeroRepetido) {
-			indexGanhador = numbers[index];
-			numeroRepetido = vezesRepete;
-			vezesRepete =0;
-		
-		}
-	}console.log(indexGanhador);
+// function numeroRepetido(numbers) {
+// 	let igual =0;
+// 	let numeroRepetido= 0;
+// 	let indexGanhador =0;
+// 	let vezesRepete =0;
+// 	for (let index = 0; index < numbers.length; index+=1) {
+// 		for (let i in numbers) {
+// 			if (numbers[index] === numbers[i]) {
+// 				igual +=1;
+// 				vezesRepete = igual;
+// 				}
+// 			}	
+// 			igual =0;
+// 			if (vezesRepete > numeroRepetido) {
+// 				indexGanhador = numbers[index];
+// 				numeroRepetido = vezesRepete;
+// 				vezesRepete =0;
+			
+// 			}
+// 		}console.log(indexGanhador);
+// }
 
+// numeroRepetido(n);
 	
-
 
 
 
