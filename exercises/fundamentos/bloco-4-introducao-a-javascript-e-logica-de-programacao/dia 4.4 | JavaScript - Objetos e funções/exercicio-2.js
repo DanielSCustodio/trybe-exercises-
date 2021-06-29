bea1ba659efeef55e7bfa8fb68a80f074224a4d9
danielm <play.daniel@hotmail.com>
2021-06-28 23:19:14 -0300

    #40 -Armazenamento do índice de um array que contém o maior valor como atributo

--- exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia 4.4 | JavaScript - Objetos e funções/exercicio-2.js
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia 4.4 | JavaScript - Objetos e funções/exercicio-2.js
@@ -1,15 +1,25 @@
-function verificaPalindrome(string) {
-	let reversed = string.split("").reverse().join(""); //split tranforma em array, reverse inverte o array e join transforma em string novamente
-		if (reversed === string) {
+// function verificaPalindrome(string) {
+// 	let reversed = string.split("").reverse().join(""); //split tranforma em array, reverse inverte o array e join transforma em string novamente
+// 		if (reversed === string) {
 			
-			console.log("true");
-		}else{
-			console.log("false");
-		}
+// 			console.log("true");
+// 		}else{
+// 			console.log("false");
+// 		}
+//  }
+// verificaPalindrome("arara");
 
-}
+// let numbers = [2, 3, 6, 7, 10, 1];
+// let maior = numbers[0];
+// let indiceMaior;
+// for (let index=0; index < numbers.length; index += 1){
+// 	if (numbers[index] > maior) {
+// 		maior = numbers[index];
+// 		indiceMaior = index
+// 	}
+// }
+// console.log(indiceMaior);
 	
 
 
 
-verificaPalindrome("arara");
