e284cdeacaefd5ee1432d422bbeaaa3773b8a76b
danielm <play.daniel@hotmail.com>
2021-07-05 19:04:43 -0300

    #83 - Adicionando uma div com classe dinamicamente

--- exercises/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ exercises/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -2,3 +2,8 @@ let body = document.querySelector('body');
 let tag = document.createElement('h1');
 tag.innerText='Exercício 5.2 - JavaScript';
 body.appendChild(tag);
+
+let div = document.createElement('div');
+div.className= 'main-content';
+
+body.appendChild(div);
