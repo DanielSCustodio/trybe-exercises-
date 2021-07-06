6245437e979553331f60bafedeff1f44f1f35197
danielm <play.daniel@hotmail.com>
2021-07-05 23:47:47 -0300

    #89 - Adicionando uma lista nao ordenada dentro de uma div

--- exercises/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ exercises/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,19 +1,19 @@
 let body = document.querySelector('body');
 let tag = document.createElement('h1');
-tag.innerText='Exercício 5.2 - JavaScript';
+tag.innerText = 'Exercício 5.2 - JavaScript';
 body.appendChild(tag);
 
 let div = document.createElement('div');
-div.className= 'main-content';
+div.className = 'main-content';
 body.appendChild(div);
 
 let divOne = document.querySelector('.main-content');
 let divTwo = document.createElement('div');
-divTwo.className='center-content';
+divTwo.className = 'center-content';
 divOne.appendChild(divTwo);
 
 let p = document.createElement('p');
-p.innerText ='Algum texto';
+p.innerText = 'Algum texto';
 divTwo.appendChild(p);
 
 let divThree = document.createElement('div');
@@ -25,7 +25,17 @@ divFour.className = 'right-content';
 divOne.appendChild(divFour)
 
 let img = document.createElement('img');
-img.className ='small-image';
-img.src='https://picsum.photos/200';
+img.className = 'small-image';
+img.src = 'https://picsum.photos/200';
 divThree.appendChild(img);
 
+let listItens =['um','dois','três','quatro','cinco','seis','sete','oito','nove','dez'];
+let ul = document.createElement('ul');
+for (let i = 0; i < listItens.length; i += 1) {
+	let item = listItens[i];
+	li = document.createElement('li');
+	li.innerText=item;
+	ul.appendChild(li);
+}
+
+divFour.appendChild(ul);
