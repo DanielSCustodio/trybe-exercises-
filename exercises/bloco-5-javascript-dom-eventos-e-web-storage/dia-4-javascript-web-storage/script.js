9aa6f9526c87333f0717875308ecc5b18b8740b9
danielm <play.daniel@hotmail.com>
2021-07-08 02:23:20 -0300

    #101 -Criaçao de uma funçao onde o usuario pode escolher a cor do texto da pagina entre 3 opcoes de cores

--- exercises/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ exercises/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -10,7 +10,18 @@ function createBoxBg() {
 	}
 }
 
+function createBoxTxt() {
+	let color = ['colorRed', 'colorGreen', 'colorBlue'];
+	for (let index = 0; index < color.length; index += 1) {
+		let div = document.createElement('div');
+		let className = color[index];
+		div.className = className + ' box';
+		//++++++++++++++++++++++++
+		let divtxt = document.getElementById('color-txt');
+		divtxt.appendChild(div)
 
+	}
+}
 
 function selectBackground() {
 	let body = document.querySelector('body');
@@ -30,8 +41,26 @@ function selectBackground() {
 		}
 	}
 }
+function selectTxt(){
+	
+	let div = document.getElementById('color-txt');
+	div.addEventListener('click', mudaCor);
 
-
+	function mudaCor(element) {
+		let txt = document.getElementById('txt');
+		let getClass = element.target.className;
+		if(getClass === 'colorRed box'){
+			txt.style.color ='red'
+		}
+		if(getClass === 'colorGreen box'){
+			txt.style.color ='green';
+		}
+		if(getClass === 'colorBlue box'){
+				txt.style.color ='blue'
+		}
+	}
+}
+selectTxt()
 createBoxBg();
-
+createBoxTxt();
 selectBackground();
