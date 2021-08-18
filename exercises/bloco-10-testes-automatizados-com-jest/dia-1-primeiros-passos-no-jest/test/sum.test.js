f4013cfe1716bb1d433308d65cc4297f44a1da18
danielm <play.daniel@hotmail.com>
2021-08-18 11:22:31 -0300

    #146 - Refazendo exercicios de Assert usando Jest- funçao sum

--- exercises/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/test/sum.test.js
+++ exercises/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/test/sum.test.js
@@ -1,6 +1,25 @@
 const sum = require('../js/sum');
 
+describe('Testes para a função de soma de 2 números', () => {
+  test('Adicionando 4+5 = 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
 
-test('Adicionando 4+5 = 9', () => {
-  expect(sum(4, 5)).toBe(9);
+  test('Adicionando 0+0= 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  test('Teste para verificar se a função lança um erro quando existe uma string como parÂmetro', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+
+  test('Teste para verificar a mensagem de erro', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow('parameters must be numbers');
+  });
+
 });
+
